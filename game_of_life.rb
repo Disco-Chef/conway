7579b1ef1a3107a94e190bd906a534745fe7b2a0
Nicolas Agathos <nicolas.agathos@gmail.com>
2021-11-06 11:53:51 +0100

    newline for readability

--- game_of_life.rb
+++ game_of_life.rb
@@ -9,7 +9,8 @@ class GameOfLife
   def run
     @view.clear_frame
     loop do
-      @grid.cells_in_grid.each { |row| row.each(&:assess_future_state!) }.each { |row| row.each(&:become_next_state!) }
+      @grid.cells_in_grid.each { |row| row.each(&:assess_future_state!) }
+           .each { |row| row.each(&:become_next_state!) }
       @view.draw_frame(@grid)
       @view.how_to_stop_message
       sleep(0.11)
